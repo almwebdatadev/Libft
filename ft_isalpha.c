8d1aa5b35d74af306d6ba328933f08d12c25231f
Alvaro Lendinez Maqueda <alendine@c2r18s6.42madrid.com>
2025-10-03 15:28:19 +0200

    finished ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,15 +6,26 @@
 /*   By: alendine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 14:41:57 by alendine          #+#    #+#             */
-/*   Updated: 2025/10/03 14:46:21 by alendine         ###   ########.fr       */
+/*   Updated: 2025/10/03 15:18:39 by alendine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <ctype.h>
 
-int main(void)
+int	ft_isalpha(int c)
 {
-	printf("%d\n", isalpha('a'));
-	printf("%d\n", isalpha('b'));
-	printf("%d\n", isalpha(0));
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
+
+int	main(void)
+{
+	printf("Df propia 'a'--> %d\n", ft_isalpha('a'));
+	printf("Df propia 'b'--> %d\n", ft_isalpha('b'));
+	printf("Df propia '0'--> %d\n", ft_isalpha('0'));
+	printf("Df sis 'a'--> %d\n", isalpha('a'));
+	printf("Df sis 'b'--> %d\n", isalpha('b'));
+	printf("Df sis '0'--> %d\n", isalpha(0));
 }
