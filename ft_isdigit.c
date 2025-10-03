f5b7f738470949c13ebd65165e8023b28e87da99
Alvaro Lendinez Maqueda <alendine@c2r18s6.42madrid.com>
2025-10-03 16:10:19 +0200

    corrected ft_isdigit.c

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,11 +6,12 @@
 /*   By: alendine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 15:29:08 by alendine          #+#    #+#             */
-/*   Updated: 2025/10/03 15:45:47 by alendine         ###   ########.fr       */
+/*   Updated: 2025/10/03 16:09:17 by alendine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <ctype.h>
 
 int	ft_isdigit(int c)
 {
@@ -24,7 +25,7 @@ int	main(void)
 	printf("RoF own 0--> %d\n", ft_isdigit('0'));
 	printf("RoF own 1--> %d\n", ft_isdigit('1'));
 	printf("RoF own a--> %d\n", ft_isdigit('a'));
-	printf("RoF system 0--> %d\n", ft_isdigit('0'));
-	printf("RoF system 1--> %d\n", ft_isdigit('1'));
-	printf("RoF system a--> %d\n", ft_isdigit('a'));
+	printf("RoF system 0--> %d\n", isdigit('0'));
+	printf("RoF system 1--> %d\n", isdigit('1'));
+	printf("RoF system a--> %d\n", isdigit('a'));
 }
